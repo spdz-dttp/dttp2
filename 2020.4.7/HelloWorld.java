5eaa1c389e3e083c988bd7a56734ca4ada8ca1a9
带头逃跑 <2455050075@qq.com>
2020-04-13 16:02:21 +0800

    测试

---
+++ 2020.4.7/HelloWorld.java
@@ -0,0 +1,54 @@
+
+/*
+如果一个类是public的 那么这个类的类名 一定要和文件名相同，
+并且一个java文件当中只能有一个共有的类
+*/
+
+
+/**
+ *
+ *
+ * 
+ */
+
+/*class A {
+
+}*/
+
+class A {
+// 
+}
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		/*
+		     1.整型int   4字节
+			 2.标识符：数字  字母  下划线  $
+		 */
+
+		int a = 0;
+
+		System.out.println(a);
+
+		// int max1 = Integer.MAX_VALUE;
+
+		// int max2 = Integer.MAX_VALUE+1;
+
+		// System.out.println(max1);
+
+		// System.out.println(max2);
+
+		// int a = 10;
+		// System.out.println(Integer.MAX_VALUE);
+		// System.out.println(Integer.MIN_VALUE);
+		// System.out.println("HelloWorld");
+
+		// System.out.println(a);//打印且换行
+		// System.out.print(a);//打印不换行
+		// System.out.printf("%d\n",a);//以格式化形式输出内容
+		
+	}
+}
